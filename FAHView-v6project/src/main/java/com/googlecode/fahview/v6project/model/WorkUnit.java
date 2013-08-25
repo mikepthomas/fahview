a4505465792e519700301ec011d67e363a29f148
montycrabapple1 <montycrabapple1@gmail.com>
2013-08-25 00:44:39 +0000

    Fixing violations

--- FAHView-v6project/src/main/java/com/googlecode/fahview/v6project/model/WorkUnit.java
+++ FAHView-v6project/src/main/java/com/googlecode/fahview/v6project/model/WorkUnit.java
@@ -30,123 +30,123 @@ public interface WorkUnit {
     // <editor-fold defaultstate="collapsed" desc="position-constants">
     /**
      * Position in bytes of the Project Number from the start of
-     * {@code queue.dat}
-     * 
-     * @see #getProj() 
-     * @see #PROJ_LENGTH 
+     * {@code queue.dat}.
+     *
+     * @see #getProj()
+     * @see #PROJ_LENGTH
      */
-    public static final int PROJ_POS = 0;
+    static final int PROJ_POS = 0;
     /**
      * Position in bytes of the Run Number from the start of
-     * {@code queue.dat}
-     * 
-     * @see #getRun() 
+     * {@code queue.dat}.
+     *
+     * @see #getRun()
      * @see #RUN_LENGTH
      */
-    public static final int RUN_POS = 2;
+    static final int RUN_POS = 2;
     /**
      * Position in bytes of the Clone Number from the start of
-     * {@code queue.dat}
-     * 
-     * @see #getClone() 
+     * {@code queue.dat}.
+     *
+     * @see #getClone()
      * @see #CLONE_LENGTH
      */
-    public static final int CLONE_POS = 4;
+    static final int CLONE_POS = 4;
     /**
      * Position in bytes of the Generation Number from the start of
-     * {@code queue.dat}
-     * 
-     * @see #getGen() 
+     * {@code queue.dat}.
+     *
+     * @see #getGen()
      * @see #GEN_LENGTH
      */
-    public static final int GEN_POS = 6;
+    static final int GEN_POS = 6;
     /**
      * Position in bytes of the Issued date from the start of
-     * {@code queue.dat}
-     * 
-     * @see #getIssue() 
+     * {@code queue.dat}.
+     *
+     * @see #getIssue()
      * @see #ISSUE_LENGTH
      */
-    public static final int ISSUE_POS = 8;
+    static final int ISSUE_POS = 8;
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="length-constants">
     /**
-     * Length in bytes of the Project Number in {@code queue.dat}
-     * 
-     * @see #getProj() 
-     * @see #PROJ_POS 
+     * Length in bytes of the Project Number in {@code queue.dat}.
+     *
+     * @see #getProj()
+     * @see #PROJ_POS
      */
-    public static final int PROJ_LENGTH = 2;
+    static final int PROJ_LENGTH = 2;
     /**
-     * Length in bytes of the Run Number in {@code queue.dat}
-     * 
-     * @see #getRun() 
-     * @see #RUN_POS 
+     * Length in bytes of the Run Number in {@code queue.dat}.
+     *
+     * @see #getRun()
+     * @see #RUN_POS
      */
-    public static final int RUN_LENGTH = 2;
+    static final int RUN_LENGTH = 2;
     /**
-     * Length in bytes of the Clone Number in {@code queue.dat}
-     * 
-     * @see #getClone() 
-     * @see #CLONE_POS 
+     * Length in bytes of the Clone Number in {@code queue.dat}.
+     *
+     * @see #getClone()
+     * @see #CLONE_POS
      */
-    public static final int CLONE_LENGTH = 2;
+    static final int CLONE_LENGTH = 2;
     /**
-     * Length in bytes of the Generation Number in {@code queue.dat}
-     * 
-     * @see #getGen() 
-     * @see #GEN_POS 
+     * Length in bytes of the Generation Number in {@code queue.dat}.
+     *
+     * @see #getGen()
+     * @see #GEN_POS
      */
-    public static final int GEN_LENGTH = 2;
+    static final int GEN_LENGTH = 2;
     /**
-     * Length in bytes of the Issued date in {@code queue.dat}
-     * 
-     * @see #getIssue() 
-     * @see #ISSUE_POS 
+     * Length in bytes of the Issued date in {@code queue.dat}.
+     *
+     * @see #getIssue()
+     * @see #ISSUE_POS
      */
-    public static final int ISSUE_LENGTH = 8;
+    static final int ISSUE_LENGTH = 8;
     // </editor-fold>
-    
+
     // <editor-fold desc="accessors">
     /**
-     * Get the value of the Project Number
+     * Get the value of the Project Number.
      *
      * @return the value of proj
      */
     int getProj();
-    
+
     /**
-     * Get the value of Run
+     * Get the value of Run.
      *
      * @return the value of run
      */
-    public int getRun();
-    
+    int getRun();
+
     /**
-     * Get the value of Clone
+     * Get the value of Clone.
      *
      * @return the value of clone
      */
-    public int getClone();
-    
+    int getClone();
+
     /**
-     * Get the value of Generation
+     * Get the value of Generation.
      *
      * @return the value of gen
      */
-    public int getGen();
-    
+    int getGen();
+
     /**
-     * Get the value of Issued
+     * Get the value of Issued.
      *
      * @return the value of issue
      */
-    public Date getIssue();
+    Date getIssue();
     // </editor-fold>
-    
+
     /**
-     * Update all values
+     * Update all values.
      */
     void update();
 }
