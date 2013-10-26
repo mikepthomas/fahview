28e0f36f07e6cb2c5303339dd2c38559fae3055f
montycrabapple1 <montycrabapple1@gmail.com>
2013-10-26 23:18:18 +0000

    Added configuration module to share plugin configuration between modules, fixing checkstyle errors and changing apt header dates to date written/updated

--- FAHView-v6project/src/main/java/com/googlecode/fahview/v6project/files/queue/QueueFileVisualElement.java
+++ FAHView-v6project/src/main/java/com/googlecode/fahview/v6project/files/queue/QueueFileVisualElement.java
@@ -2,23 +2,22 @@ package com.googlecode.fahview.v6project.files.queue;
 
 /*
  * #%L
- * FAHView-v6project
+ * This file is part of FAHView-v6project.
  * %%
  * Copyright (C) 2011 - 2013 Michael Thomas <mikepthomas@outlook.com>
  * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ * FAHView is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * %
+ * FAHView is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * %
+ * You should have received a copy of the GNU General Public License
+ * along with FAHView.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
---
+++ configuration/src/main/resources/license/description-template.ftl
@@ -0,0 +1,25 @@
+<#--
+  #%L
+ This file is part of FAHView-configuration.
+ %%
+ Copyright (C) 2011 - 2013 Michael Thomas <mikepthomas@outlook.com>
+ %%
+ FAHView is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ %
+ FAHView is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ %
+ You should have received a copy of the GNU General Public License
+ along with FAHView.  If not, see <http://www.gnu.org/licenses/>.
+ #L%
+  -->
+This file is part of ${projectName}.
+<#if addSvnKeyWords>
+${"|Id:|"?replace("|", "$")}
+${"|HeadURL:|"?replace("|", "$")}
+</#if>
